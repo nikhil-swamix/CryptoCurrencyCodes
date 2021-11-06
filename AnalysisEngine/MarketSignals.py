b96a618f9810a9c19c3c99450a7f55d9161c1cbe
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2021-11-07 00:51:45 +0530

    organize

--- AnalysisEngine/MarketSignals.py
+++
@@ -1,4 +0,0 @@
-from mxproxy import mx
-
-source='https://www.centralcharts.com/en/6863-btc-usd/signals'
-blobk='signal-desc'
--- AnalysisEngine/niceHashProfitabilityAPI.py
+++
@@ -1,2 +0,0 @@
-from mxproxy import requests
-ENDPOINT="https://api2.nicehash.com/main/api/v2/public/profcalc/devices"
